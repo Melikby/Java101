2a7431c3cb267da6d2dc3bac88fb96ddb5edb90f
Melikby <109859012+Melikby@users.noreply.github.com>
2022-08-17 15:49:38 +0300

    Add files via upload

---
+++ Matris Transpozunu Bulma/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Matris'in kaç satırı olacak: ");
+        int length = inp.nextInt();
+        System.out.print("Matris'in kaç sutunu olacak: ");
+        int weight = inp.nextInt();
+        System.out.println("Matrisin elemanlarını sırasıyla giriniz : ");
+        int[][] matrix = new int[length][weight];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++)
+                matrix[i][j] = inp.nextInt();
+        }
+        System.out.println("======================");
+        System.out.println("Matris : ");
+        System.out.println();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++)
+                System.out.print(matrix[i][j] + "   ");
+            System.out.println();
+        }
+
+        System.out.println("======================");
+        System.out.println("Matrisin transpozu : ");
+        System.out.println();
+        for (int j = 0; j < matrix[0].length; j++) {
+            for (int i = 0; i < matrix.length; i++) {
+                System.out.print(matrix[i][j] + "   ");
+            }
+            System.out.println();
+            inp.close();
+        }
+    }
+}
